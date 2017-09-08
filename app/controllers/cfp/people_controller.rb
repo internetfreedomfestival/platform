bd10f132b161a8aa64ae93b9461f984ec0cd3b6f
Jamie MacKillop <jamieronin@gmail.com>
2017-09-08 16:42:38 -0400

    Add flash message and redirect to cfp/events to deny access for event submission for users without required attributes. Remove availability option for events in Cfp::PeopleController. Add new person model attire to person_params in PeopleController.

--- app/controllers/cfp/people_controller.rb
+++ app/controllers/cfp/people_controller.rb
@@ -7,10 +7,6 @@ class Cfp::PeopleController < ApplicationController
   def show
     @person = current_user.person
 
-    if not @conference.in_the_past and @person.events_in(@conference).size > 0 and @person.availabilities_in(@conference).count == 0
-      flash[:alert] = t('cfp.specify_availability')
-    end
-
     return redirect_to action: 'new' unless @person
     if @person.public_name == current_user.email
       flash[:alert] = 'Your email address is not a valid public name, please change it.'
@@ -55,7 +51,6 @@ class Cfp::PeopleController < ApplicationController
 
   def update
     @person = current_user.person
-
     respond_to do |format|
       if @person.update_attributes(person_params)
         format.html { redirect_to(cfp_person_path, notice: t('cfp.person_updated_notice')) }
@@ -71,11 +66,11 @@ class Cfp::PeopleController < ApplicationController
 
   def person_params
     params.require(:person).permit(
-      :first_name, :last_name, :public_name, :email, :email_public, :gender, :avatar, :abstract, :description, :include_in_mailings,
+      :first_name, :last_name, :public_name, :email, :email_public, :gender, :avatar, :abstract, :description, :include_in_mailings, :include_in_mailings, :pgp_key, :country_of_origin, :professional_background, :other_background, :organization, :project, :title, :iff_before, :invitation_to_mattermost, :interested_in_volunteer,
       im_accounts_attributes: %i(id im_type im_address _destroy),
       languages_attributes: %i(id code _destroy),
       links_attributes: %i(id title url _destroy),
-      phone_numbers_attributes: %i(id phone_type phone_number _destroy)
+      phone_numbers_attributes: %i(id phone_type phone_number _destroy),
     )
   end
 end
