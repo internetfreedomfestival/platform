6b4b7ac6c327282723966c1b7cd74ac079634895
Jamie MacKillop <jamieronin@gmail.com>
2018-01-16 18:16:20 -0500

    Add person id variable to mass emails

--- app/models/mail_template.rb
+++ app/models/mail_template.rb
@@ -9,6 +9,7 @@ class MailTemplate < ActiveRecord::Base
       .gsub('#first_name',  user.first_name)
       .gsub('#last_name',   user.last_name)
       .gsub('#public_name', user.public_name)
+      .gsub('#person_id', user.id.to_s)
       .gsub('#confirm_attendance', "https://platform.internetfreedomfestival.org/en/IFF2018/cfp/user/confirmation/confirm_attendance?confirm_attendance_token=#{user.user.confirm_attendance_token}")
   end
 
