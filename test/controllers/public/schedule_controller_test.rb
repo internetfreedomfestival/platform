d839a52b743e18e0c80660af2f25105f4f7d972f
Vicente Baixauli <vibaiher@gmail.com>
2018-07-24 00:39:55 +0200

    Schedule controller only redirects to custom path

--- test/controllers/public/schedule_controller_test.rb
+++ test/controllers/public/schedule_controller_test.rb
@@ -5,56 +5,24 @@ class Public::ScheduleControllerTest < ActionController::TestCase
     @conference = create(:three_day_conference_with_events)
   end
 
-  test 'displays schedule main page' do
+  test 'redirects schedule main page to public custom path' do
     get :index, conference_acronym: @conference.acronym
-    assert_response :success
+    assert_response :redirect
   end
 
-  test 'displays xml schedule' do
-    get :index, format: :xml, conference_acronym: @conference.acronym
-    assert_response :success
-  end
-
-  test 'displays json schedule' do
-    get :index, format: :json, conference_acronym: @conference.acronym
-    assert_response :success
-  end
-
-  test 'displays ical schedule' do
-    get :index, format: :ics, conference_acronym: @conference.acronym
-    assert_response :success
-  end
-
-  test 'displays xcal schedule' do
-    get :index, format: :xcal, conference_acronym: @conference.acronym
-    assert_response :success
-  end
-
-  test 'displays schedule for a day' do
+  test 'redirects schedule for a day to public custom path' do
     get :day, day: 1, conference_acronym: @conference.acronym
-    assert_response :success
+    assert_response :redirect
   end
 
-  test 'displays pdf schedule for a day' do
-    get :day, day: 1, conference_acronym: @conference.acronym, format: 'pdf'
-    assert_response :success
-  end
-
-  test 'display first day' do
-    get :day, day: 1, conference_acronym: @conference.acronym, format: 'pdf'
-    assert_response :success
-  end
-
-  test 'display an event' do
+  test 'redirects an event to public custom path' do
     get :events, id: 1, conference_acronym: @conference.acronym
-    assert_response :success
+    assert_response :redirect
   end
 
-  test 'displays events list' do
+  test 'redirects events list to public custom path' do
     get :events, conference_acronym: @conference.acronym
-    assert_response :success
-    get :events, conference_acronym: @conference.acronym, format: :xls
-    assert_response :success
+    assert_response :redirect
   end
 
   test 'displays speakers list' do
@@ -68,33 +36,4 @@ class Public::ScheduleControllerTest < ActionController::TestCase
     get :speakers, id: 1, conference_acronym: @conference.acronym
     assert_response :success
   end
-
-  test 'json schedule contains the conference events' do
-    get :events, format: :json, conference_acronym: @conference.acronym
-    assert_response :success
-
-    data = JSON.parse(@response.body)
-    events = data['conference_events']['events']
-
-    assert_equal events.count, @conference.events.count
-  end
-
-  test 'json schedule contains the sub-conference events' do
-    subc = @conference.subs.first
-    subc.rooms << create(:room, conference: subc)
-    subc.events << create(:event, conference: subc,
-                                  room: subc.rooms.first,
-                                  state: 'confirmed',
-                                  public: true,
-                                  start_time: Date.today.since(1.days).since(11.hours))
-
-    get :events, format: :json, conference_acronym: @conference.acronym
-    assert_response :success
-
-    data = JSON.parse(@response.body)
-    events = data['conference_events']['events']
-
-    assert_equal events.count, @conference.events.count + subc.events.count
-  end
-
 end
