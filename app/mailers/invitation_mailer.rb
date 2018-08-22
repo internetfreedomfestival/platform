16cda030ce08ada7ea9e8bf2d41cbbfa37d5d68e
helenmg <elenamg31@gmail.com>
2018-08-22 13:49:31 +0200

    #95Create mail template for addittional invitees

--- app/mailers/invitation_mailer.rb
+++ app/mailers/invitation_mailer.rb
@@ -2,10 +2,20 @@ class InvitationMailer < ApplicationMailer
   def invitation_mail(invited)
     @person = invited.person
     @conference = invited.conference
-    @link = ticketing_form_url(id: @person.id, conference_acronym: @conference.acronym)
+    @link = ticketing_form_url(id: invited.id, conference_acronym: @conference.acronym)
     mail(
       to: @person.email,
       subject: I18n.t('emails.invitation_mail.subject'),
+      locale: :en
+    )
+  end
+  def additional_invitation_mail(invited)
+    @person = invited.person
+    @conference = invited.conference
+    @link = ticketing_form_url(id: invited.id, conference_acronym: @conference.acronym)
+    mail(
+      to: "invited@email.com",
+      subject: I18n.t('emails.additional_invitation_mail.subject'),
       locale: :en
     )
   end
