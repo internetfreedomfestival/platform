1b50b484062a3d933d31ffb79623261c411238c2
helenmg <elenamg31@gmail.com>
2018-08-22 13:56:11 +0200

    fix test

--- test/mailers/invitation_mailer_test.rb
+++ test/mailers/invitation_mailer_test.rb
@@ -6,7 +6,7 @@ class InvitationMailerTest < ActionMailer::TestCase
 
     email = InvitationMailer.invitation_mail(invited).deliver_now
 
-    expected_link = "#{invited.conference.acronym}/invitations/#{invited.person.id}/ticketing_form"
+    expected_link = "#{invited.conference.acronym}/invitations/#{invited.id}/ticketing_form"
     assert_not ActionMailer::Base.deliveries.empty?
     assert_equal [ENV.fetch('FROM_EMAIL')], email.from
     assert_equal [invited.person.email], email.to
