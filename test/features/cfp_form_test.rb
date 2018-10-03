e7dd543d62c24fbc1869c1a92582d2a6715b4aa3
Mikel Etxeberria <mikelxabier@gmail.com>
2018-10-03 18:35:26 +0200

    Tests Event

--- test/features/cfp_form_test.rb
+++ test/features/cfp_form_test.rb
@@ -43,6 +43,69 @@ class CfpFormTest < Capybara::Rails::TestCase
     assert_text 'Your proposal was successfully created.'
   end
 
+  test 'an user can edit a call for proposals' do
+    visit '/'
+
+    event = create(:event, conference: @conference)
+    create(:event_person, event: event, person: @user.person, event_role: "submitter")
+    create(:event_person, event: event, person: @user.person, event_role: "speaker")
+
+    login_as(@user)
+
+    visit "/#{@conference.acronym}/cfp/events/#{event.id}/edit"
+
+    within '#cfp_form' do
+      check('event[instructions]', option: 'true')
+      fill_in 'event[title]', with: 'Session Title'
+      fill_in 'event[subtitle]', with: 'Subtitle Event'
+      fill_in 'event[description]', with: 'Session description'
+      fill_in 'event[other_presenters]', with: @person.email
+      fill_in 'event[public_type]', with: 'Students'
+      fill_in 'event[desired_outcome]', with: 'desired_outcome'
+      fill_in 'event[phone_number]', with: 12345678
+      select('Feature', from: 'event[track_id]')
+      select('On the Frontlines', from: 'event[event_type]')
+      choose 'Yes'
+      check('event[iff_before][]', option: '2018')
+      check('event[code_of_conduct]', option: 'true')
+
+      click_on 'Update Proposal'
+    end
+
+    assert_text 'Your proposal was successfully updated.'
+  end
+
+  test 'an user can view their call of proposals' do
+    visit '/'
+
+    event = create(:event, conference: @conference)
+    create(:event_person, event: event, person: @user.person, event_role: "submitter")
+    create(:event_person, event: event, person: @user.person, event_role: "speaker")
+
+    login_as(@user)
+
+    visit "/#{@conference.acronym}/cfp/person"
+    assert_text event.title
+  end
+
+
+  test 'an user can delete a call for proposals' do
+    visit '/'
+
+    event = create(:event, conference: @conference)
+    create(:event_person, event: event, person: @user.person, event_role: "submitter")
+    create(:event_person, event: event, person: @user.person, event_role: "speaker")
+
+    login_as(@user)
+
+    visit "/#{@conference.acronym}/cfp/person"
+
+    click_on 'Delete'
+
+    assert_text "Your proposal: '#{event.title}' has been deleted"
+  end
+
+
   private
 
   def login_as(user)
