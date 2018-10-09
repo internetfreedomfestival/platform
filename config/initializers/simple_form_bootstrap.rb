60dc51c3eebea276e1c664228a033ddc2b2cb8f1
Eduardo Sebastian <esebastian@altiora.es>
2018-10-10 00:59:38 +0200

    Use legacy design in sign in form

--- config/initializers/simple_form_bootstrap.rb
+++ config/initializers/simple_form_bootstrap.rb
@@ -2,12 +2,29 @@
 SimpleForm.setup do |config|
   config.wrappers :horizontal do |b|
     b.use :html5
-    b.wrapper tag: 'div', class: 'form-input' do |ba|
+    b.wrapper tag: 'div', class: 'input' do |ba|
       ba.use :input
     end
   end
 
-  config.wrappers :horizontal_string, tag: 'div', class: 'clearfix form-group', error_class: 'has-error' do |b|
+  config.wrappers :horizontal_string, tag: 'div', class: 'clearfix stringish', error_class: 'has-error' do |b|
+    b.use :html5
+    b.use :placeholder
+    b.optional :maxlength
+    b.optional :pattern
+    b.optional :min_max
+    b.optional :readonly
+    b.use :label
+    b.use :error, wrap_with: { tag: :span, class: :error }
+
+    # TODO * not shown on password field
+    b.wrapper tag: 'div', class: 'input' do |ba|
+      ba.use :input
+      ba.use :hint, wrap_with: { tag: :span, class: 'help-block' }
+    end
+  end
+
+  config.wrappers :default, tag: 'div', class: 'clearfix form-group', error_class: 'has-error' do |b|
     b.use :html5
     b.use :placeholder
     b.optional :maxlength
@@ -29,41 +46,36 @@ SimpleForm.setup do |config|
     end
   end
 
-  config.wrappers :horizontal_boolean, tag: 'div', class: 'clearfix form-group', error_class: 'has-error' do |b|
+  config.wrappers :horizontal_boolean, tag: 'div', class: 'clearfix', error_class: 'has-error' do |b|
     b.use :html5
     b.optional :readonly
     b.use :label
 
-    b.wrapper tag: 'div', class: 'form-help' do |ba|
-      ba.use :hint, wrap_with: { tag: :span, class: 'form-help__text' }
-    end
-
-    b.wrapper tag: 'div', class: 'form-input' do |ba|
+    # TODO right align, margin, missing ul<li
+    b.wrapper tag: 'div', class: 'input' do |ba|
       ba.use :input
+      ba.use :hint, wrap_with: { tag: :span, class: 'help-block' }
     end
   end
 
-  config.wrappers :check_boxes, tag: 'div', class: 'inputs-list clearfix form-group', error_class: 'has-error' do |b|
+  config.wrappers :check_boxes, tag: 'div', class: 'inputs-list clearfix', error_class: 'has-error' do |b|
     b.use :html5
     b.optional :readonly
     b.use :label
     b.use :error, wrap_with: { tag: :span, class: :error }
 
-    b.wrapper tag: 'div', class: 'form-help' do |ba|
-      ba.use :hint, wrap_with: { tag: :span, class: 'form-help__text' }
-    end
-
-    b.wrapper tag: 'div', class: 'form-input' do |ba|
+    b.wrapper tag: 'div', class: 'input' do |ba|
       ba.use :input, wrap_with: { tag: :div, class: 'clearfix' }
+      ba.use :hint, wrap_with: { tag: :span, class: 'help-block' }
     end
   end
 
-  config.default_wrapper = :horizontal_string
+  config.default_wrapper = :default
 
   # http://simple-form.plataformatec.com.br/#available-input-types-and-defaults-for-each-column-type
   config.wrapper_mappings = {
     inline_boolean: :horizontal_boolean,
-    email: :horizontal_string,
+    #email: :horizontal_string,
     rating: :horizontal,
     check_boxes: :check_boxes,
     hidden: :horizontal
