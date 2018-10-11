a4b96946ae585821dc729eb0160d2b8401033b1d
Mikel Etxeberria <mikelxabier@gmail.com>
2018-10-11 17:24:45 +0200

    test verify duplicated email on registration of user

--- test/controllers/cfp/users_controller_test.rb
+++ test/controllers/cfp/users_controller_test.rb
@@ -34,6 +34,34 @@ class Cfp::UsersControllerTest < ActionController::TestCase
     assert_not_nil user.confirmation_token
   end
 
+  test 'denies the registration of users with same email' do
+    sign_up_form_params = {
+      email: 'new_user@example.com',
+      email_confirmation: 'new_user@example.com',
+      password: 'frab1234',
+      password_confirmation: 'frab1234',
+      first_name: 'Aretha',
+      gender: 'Female',
+      country_of_origin: 'United States',
+      professional_background: ['Software/web developer'],
+      include_in_mailings: true,
+      invitation_to_mattermost: true
+    }
+    post :create, conference_acronym: @conference.acronym, sign_up_form: sign_up_form_params
+
+    assert_response :redirect
+
+    items_found = User.find_by(email: 'new_user@example.com')
+
+    duplicated_params = sign_up_form_params
+    post :create, conference_acronym: @conference.acronym, sign_up_form: duplicated_params
+
+    items_found_after_duplicated_insertion = User.find_by(email: 'new_user@example.com')
+
+    assert_equal items_found, items_found_after_duplicated_insertion
+  end
+
+
   test 'shows password editing form' do
     login_as(:submitter)
     get :edit, conference_acronym: @conference.acronym
