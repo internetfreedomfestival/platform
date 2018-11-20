5eb5dcc469831e57085a9ff13a5843106d46c843
helenmg <elenamg31@gmail.com>
2018-11-20 18:00:40 +0100

    db migration to dif_status

---
+++ db/migrate/20181120164805_set_dif_status_to_requested.rb
@@ -0,0 +1,16 @@
+class SetDifStatusToRequested < ActiveRecord::Migration
+  def up
+    conference = Conference.find_by(acronym: 'IFF2019')
+    events = Event.where(conference: conference, travel_assistance: true)
+
+    events.each do |event|
+      Rails.logger.info "Updating event: #{event.id}. Add value: Requested, to dif_status"
+      event.dif_status = "Requested"
+      event.save
+    end
+  end
+
+  def down
+    Rails.logger.info "Nothing to do here!"
+  end
+end
