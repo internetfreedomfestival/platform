f4096510f31e42a7693b6c73d2530837f080e5c8
Eduardo Sebastian <esebastian@altiora.es>
2018-11-29 20:19:14 +0100

    Update invitation mailer tests

--- test/mailers/invitation_mailer_test.rb
+++ test/mailers/invitation_mailer_test.rb
@@ -1,44 +1,58 @@
 require 'test_helper'
 
 class InvitationMailerTest < ActionMailer::TestCase
-  test "sends invitation mail to the invited email with the ticket links" do
-    invited = create(:invited)
-    create(:person, email:invited.email)
+  test 'sends invitation mail to the invited email with the ticket links' do
+    invitee = create(:invited)
+    create(:person, email: invitee.email)
 
-    email = InvitationMailer.invitation_mail(invited).deliver_now
+    email = InvitationMailer.invitation_mail(invitee).deliver_now
 
-    expected_link = "#{invited.conference.acronym}/invitations/#{invited.id}/ticketing_form"
+    expected_link = "#{invitee.conference.acronym}/invitations/#{invitee.id}/ticketing_form"
     assert_not ActionMailer::Base.deliveries.empty?
     assert_equal [ENV.fetch('FROM_EMAIL')], email.from
-    assert_equal [invited.email], email.to
-    assert_equal "You are invited to the 2019 IFF!", email.subject
+    assert_equal [invitee.email], email.to
+    assert_equal 'You are invited to claim an IFF Ticket!', email.subject
     assert_match expected_link, email.body.to_s
   end
 
-  test "send the invitation to the user who made the request for the ticket" do
-    invited = create(:invited)
-    create(:person, email:invited.email)
+  test 'send the invitation to the user who made the request for the ticket' do
+    invitee = create(:invited)
+    create(:person, email: invitee.email)
 
-    email = InvitationMailer.accept_request_mail(invited).deliver_now
+    email = InvitationMailer.accept_request_mail(invitee).deliver_now
 
-    expected_link = "#{invited.conference.acronym}/invitations/#{invited.id}/ticketing_form"
+    expected_link = "#{invitee.conference.acronym}/invitations/#{invitee.id}/ticketing_form"
     assert_not ActionMailer::Base.deliveries.empty?
     assert_equal [ENV.fetch('FROM_EMAIL')], email.from
-    assert_equal [invited.email], email.to
-    assert_equal "Here’s your invite to the 2019 IFF!", email.subject
+    assert_equal [invitee.email], email.to
+    assert_equal 'Here’s your invite to the 2019 IFF!', email.subject
     assert_match expected_link, email.body.to_s
   end
 
-  test "sends invitation mail to non registred users" do
-    invited = create(:invited)
+  test 'sends invitation mail to non registered users' do
+    invitee = create(:invited)
 
-    email = InvitationMailer.not_registered_invitation_mail(invited).deliver_now
+    email = InvitationMailer.not_registered_invitation_mail(invitee).deliver_now
 
-    expected_link = "#{invited.conference.acronym}/invitations/#{invited.id}/ticketing_form"
+    expected_link = "#{invitee.conference.acronym}/invitations/#{invitee.id}/ticketing_form"
     assert_not ActionMailer::Base.deliveries.empty?
     assert_equal [ENV.fetch('FROM_EMAIL')], email.from
-    assert_equal [invited.email], email.to
-    assert_equal "You are invited to the 2019 IFF!", email.subject
+    assert_equal [invitee.email], email.to
+    assert_equal 'You are invited to claim an IFF Ticket!', email.subject
+    assert_match expected_link, email.body.to_s
+  end
+
+  test 'sends invitation mail from a regular user' do
+    invitee = create(:invited)
+    inviter = invitee.person
+
+    email = InvitationMailer.additional_invitation_mail(invitee).deliver_now
+
+    expected_link = "#{invitee.conference.acronym}/invitations/#{invitee.id}/ticketing_form"
+    assert_not ActionMailer::Base.deliveries.empty?
+    assert_equal [ENV.fetch('FROM_EMAIL')], email.from
+    assert_equal [invitee.email], email.to
+    assert_equal "#{inviter.first_name} invited you to get an IFF Ticket!", email.subject
     assert_match expected_link, email.body.to_s
   end
 end
